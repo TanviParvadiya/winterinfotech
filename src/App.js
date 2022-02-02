cbd4e9a4ef83ead2e34d65c5e180b4bcd56cb95a
TanviParvadiya <tanviparvadiya94709@gmail.com>
2022-02-02 15:42:44 +0530

    Commit changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './theme/css/App.css';
+import Index from '../src/theme/Index'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <Index/>
+  );
+}
+
+export default App;
